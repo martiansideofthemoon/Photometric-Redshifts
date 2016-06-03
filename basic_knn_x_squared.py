06877e25cddd4d2f517fc3e61739679c1bf46abb
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-06-04 00:27:16 +0530

    adding ML code to train x^2

--- basic_knn_x_squared.py
+++ basic_knn_x_squared.py
@@ -1,20 +1,19 @@
 import numpy as np
+import random
 import matplotlib.pyplot
 
 # Test parameters
-TRAINING_SIZE = 100000
-TEST_SIZE = 100
-K = 100
+TRAINING_SIZE = 5
+TEST_SIZE = 1000
+K = 2
 
 
 def distance(training_data, test_vector):
-	return np.sum((training_data - test_vector)**2, axis=1)**0.5
-
-my_data = np.genfromtxt('psf_z.csv', delimiter=',')[1:, :]
+	return ((training_data - test_vector)**2)**0.5
 
 # Training data and corresponding z values
-training_data = my_data[:TRAINING_SIZE, 3:]
-labels = my_data[:TRAINING_SIZE, 1]
+training_data = np.linspace(1, 1000, num=TRAINING_SIZE)
+labels = training_data**2
 
 # Will contain final points
 points = np.zeros([TEST_SIZE, 2])
@@ -23,9 +22,9 @@ for i in range(0, TEST_SIZE):
 	if i%10 == 0:
 		print i
 	# A test vector containing five test parameters
-	test_vector = my_data[TRAINING_SIZE+i, 3:]
+	test_vector = random.randrange(0, 1000000)/1000.0
 	# Expected z shift value
-	expected_label = my_data[TRAINING_SIZE+i, 1]
+	expected_label = test_vector**2
 	points[i, 1] = expected_label
 
 	# Finding distance of each row of training data with test vector
@@ -36,7 +35,7 @@ for i in range(0, TEST_SIZE):
 	v = v[v[:, 1].argsort()]
 	v = v[:K, :]
 
-	# Finding weighted average of redshift with 1/distance as weights
+	# Finding weighted average of x^2 with 1/distance as weights
 	points[i, 0] = np.sum(v[:,0]*np.reciprocal(v[:,1]))/np.sum(np.reciprocal(v[:,1]))
 
 matplotlib.pyplot.scatter(points[:,0], points[:,1])
