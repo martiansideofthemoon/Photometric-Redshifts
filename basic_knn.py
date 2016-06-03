2ab7427e86266306800828671c34073c9dea2c4f
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-06-03 23:52:57 +0530

    Adding comments to script

--- basic_knn.py
+++ basic_knn.py
@@ -1,28 +1,39 @@
 import numpy as np
 import matplotlib.pyplot
 
-TRAINING_SIZE = 10000
-TEST_SIZE = 1000
-K = 1000
+# Test parameters
+TRAINING_SIZE = 100000
+TEST_SIZE = 100
+K = 100
 
 my_data = np.genfromtxt('psf_z.csv', delimiter=',')[1:, :]
 
+# Training data and corresponding z values
 training_data = my_data[:TRAINING_SIZE, 3:]
 labels = my_data[:TRAINING_SIZE, 1]
+
+# Will contain final points
 points = np.zeros([TEST_SIZE, 2])
 
 for i in range(0, TEST_SIZE):
 	if i%10 == 0:
 		print i
+	# A test vector containing five test parameters
 	test_data = my_data[TRAINING_SIZE+i, 3:]
-	expected_labels = my_data[TRAINING_SIZE+i, 1]
+	# Expected z shift value
+	expected_label = my_data[TRAINING_SIZE+i, 1]
+	points[i, 1] = expected_label
+
+	# Finding distance of each row of training data with test vector
 	v = np.sum((training_data - test_data)**2, axis=1)**0.5
+
+	# Taking k nearest neigbours
 	v = np.column_stack((labels, v))
 	v = v[v[:, 1].argsort()]
 	v = v[:K, :]
+
+	# Finding weighted average of redshift with 1/distance as weights
 	points[i, 0] = np.sum(v[:,0]*np.reciprocal(v[:,1]))/np.sum(np.reciprocal(v[:,1]))
-	points[i, 1] = expected_labels
 
 matplotlib.pyplot.scatter(points[:,0], points[:,1])
-
 matplotlib.pyplot.show()
