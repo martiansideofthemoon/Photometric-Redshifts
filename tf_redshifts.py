a7468db85ebfefa86f8bac514eadd2c0bd1daf30
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2016-10-28 17:59:28 +0530

    Adding RMSProp and multiple epochs, still doesn't work

--- tf_redshifts.py
+++ tf_redshifts.py
@@ -11,12 +11,13 @@ NUM_CLASSES = 1
 # Basic model parameters as external flags.
 flags = tf.app.flags
 FLAGS = flags.FLAGS
-flags.DEFINE_float('learning_rate', 0.005, 'Initial learning rate.')
+flags.DEFINE_float('learning_rate', 0.001, 'Initial learning rate.')
 flags.DEFINE_integer('max_steps', 2000, 'Number of steps to run trainer.')
-flags.DEFINE_integer('hidden1', 20, 'Number of units in hidden layer 1.')
-flags.DEFINE_integer('hidden2', 32, 'Number of units in hidden layer 2.')
+flags.DEFINE_integer('hidden1', 100, 'Number of units in hidden layer 1.')
+flags.DEFINE_integer('hidden2', 30, 'Number of units in hidden layer 2.')
 flags.DEFINE_integer('batch_size', 100, 'Batch size.  '
                      'Must divide evenly into the dataset sizes.')
+flags.DEFINE_integer('num_epoch', 40, 'Epoch size')
 flags.DEFINE_string('train_dir', 'data', 'Directory to put the training data.')
 flags.DEFINE_boolean('fake_data', False, 'If true, uses fake data '
                      'for unit testing.')
@@ -25,6 +26,7 @@ batch_size = FLAGS.batch_size
 hidden_size1 = FLAGS.hidden1
 hidden_size2 = FLAGS.hidden2
 learning_rate = FLAGS.learning_rate
+num_epoch = FLAGS.num_epoch
 
 print "Reading data..."
 data = np.genfromtxt("data/psf2.csv",delimiter=",")[2:,1:]
@@ -40,8 +42,19 @@ assert X.shape[1] == 5
 # the output
 Y = data[:,:1]
 
-test_X = X[-10000:,:]
-test_Y = Y[-10000:,:]
+print "Normalizing data..."
+minx, maxx = np.min(X), np.max(X)
+miny, maxy = np.min(Y), np.max(Y)
+
+X = (X-minx)/(maxx-minx)
+Y = (Y-miny)/(maxy-miny)
+print "Data normalized..."
+
+training_X = X[:100000,:]
+training_Y = Y[:100000,:]
+
+test_X = X[100000:110000,:]
+test_Y = Y[100000:110000,:]
 
 with tf.Graph().as_default():
 
@@ -69,15 +82,15 @@ with tf.Graph().as_default():
                            name='biases')
       logits = tf.matmul(hidden2, weights) + biases
 
-  # with tf.name_scope('sigmoid'):
-  #   logits = tf.sigmoid(logits)
+  with tf.name_scope('sigmoid'):
+    logits = tf.sigmoid(logits)
 
-  loss = tf.sqrt(tf.reduce_mean(tf.square(tf.sub(logits, z_placeholder))))
-  eval_model = tf.reduce_sum(tf.square(tf.sub(logits, z_placeholder)))
+  loss = tf.reduce_mean(tf.squared_difference(logits, z_placeholder))
+
   # Add a scalar summary for the snapshot loss.
   tf.scalar_summary(loss.op.name, loss)
-  # Create the gradient descent optimizer with the given learning rate.
-  optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+  # Create the rmsprop optimizer with the given learning rate.
+  optimizer = tf.train.RMSPropOptimizer(learning_rate)
   # Create a variable to track the global step.
   global_step = tf.Variable(0, name='global_step', trainable=False)
   # Use the optimizer to apply the gradients that minimize the loss
@@ -89,20 +102,21 @@ with tf.Graph().as_default():
 
   sess.run(init)
 
-  for step in range(0, FLAGS.max_steps):
+  steps_per_epoch = len(training_X) / batch_size
+  print str(steps_per_epoch) + " steps per epoch"
+
+  for epoch in range(0, num_epoch):
     start_time = time.time()
-    feed_dict = {
-      photo_placeholder: X[step*batch_size:(step+1)*batch_size],
-      z_placeholder: Y[step*batch_size:(step+1)*batch_size,0]
-    }
-    _, loss_value = sess.run([train_op, loss], feed_dict=feed_dict)
+    for step in range(0, steps_per_epoch):
+      feed_dict = {
+        photo_placeholder: training_X[step*batch_size:(step+1)*batch_size],
+        z_placeholder: training_Y[step*batch_size:(step+1)*batch_size,0]
+      }
+      _, loss_value, biases1 = sess.run([train_op, loss, biases], feed_dict=feed_dict)
     duration = time.time() - start_time
+    print('Epoch %d: loss = %.5f (%.3f sec)' % (epoch, loss_value, duration))
 
-    if step % 100 == 0:
-      # Print status to stdout.
-      print('Step %d: loss = %.2f (%.3f sec)' % (step, loss_value, duration))
-
-  steps_per_epoch = 10000 / 100
+  steps_per_epoch = len(test_X) / batch_size
   total = []
   for step in range(0, steps_per_epoch):
     feed_dict = {
